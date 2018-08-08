677a4ab2acc11330e80182f0d6a3cdd0d33287d7
Keanu Lee <keanulee517@gmail.com>
2018-08-08 15:50:42 -0700

    Fix MD table in paper-card.js

--- paper-card.js
+++ paper-card.js
@@ -49,16 +49,12 @@ The following custom properties and mixins are available for styling:
 
 Custom property | Description | Default
 ----------------|-------------|----------
-`--paper-card-background-color` | The background color of the card |
-`--primary-background-color`
+`--paper-card-background-color` | The background color of the card | `--primary-background-color`
 `--paper-card-header-color` | The color of the header text | `#000`
 `--paper-card-header` | Mixin applied to the card header section | `{}`
-`--paper-card-header-text` | Mixin applied to the title in the card header
-section | `{}`
-`--paper-card-header-image` | Mixin applied to the image in the card header
-section | `{}`
-`--paper-card-header-image-text` | Mixin applied to the text overlapping the
-image in the card header section | `{}`
+`--paper-card-header-text` | Mixin applied to the title in the card header section | `{}`
+`--paper-card-header-image` | Mixin applied to the image in the card header section | `{}`
+`--paper-card-header-image-text` | Mixin applied to the text overlapping the image in the card header section | `{}`
 `--paper-card-content` | Mixin applied to the card content section| `{}`
 `--paper-card-actions` | Mixin applied to the card action section | `{}`
 `--paper-card` | Mixin applied to the card | `{}`
